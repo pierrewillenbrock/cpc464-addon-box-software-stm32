c494ed1bec6e4fc305f4f86d449750053705f955
Pierre Willenbrock <pierre@pirsoft.de>
2017-05-14 20:14:10 +0200

    Add repetition counting to eventlogger

--- stm32/src/eventlogger.cpp
+++ stm32/src/eventlogger.cpp
@@ -6,8 +6,10 @@
 
 struct LogInfo {
   const char *info;
-  uint64_t timestamp;
   void *extra;
+  uint64_t time;
+  uint32_t rep;
+  uint32_t timesince;
 };
 
 std::array<LogInfo,512> eventlog;
@@ -15,17 +17,26 @@ unsigned int eventlogpos = 0;
 
 void LogEvent(const char *info, void *extra) {
   unsigned pos;
+  uint64_t now;
   {
     ISR_Guard g;
+    now = Timer_timeSincePowerOn();
     pos = eventlogpos;
+    unsigned ppos = pos == 0?eventlog.size()-1:pos-1;
+    if (eventlog[ppos].info == info && eventlog[ppos].extra == extra) {
+	eventlog[ppos].rep++;
+	eventlog[ppos].timesince = now - eventlog[ppos].time;
+	return;
+    }
     eventlogpos++;
     if (eventlogpos >= eventlog.size())
       eventlogpos = 0;
   }
+
   LogInfo &i = eventlog[pos];
   i.info = info;
   i.extra = extra;
-  i.timestamp = Timer_timeSincePowerOn();
+  i.time = now;
 }
 
 void LogEvent(const char *info) {
